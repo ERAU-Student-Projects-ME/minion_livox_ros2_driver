551ec22f548b2a2c29b716442fedc7169fc1e48d
Sagar Sarkar <sarkars1@my.erau.edu>
2025-10-10 17:29:51 -0400

    Added separate config, launch file for each Lidat

--- livox_ros2_driver/launch/livox_minion_launch.py
+++ livox_ros2_driver/launch/livox_minion_launch.py
@@ -16,39 +16,70 @@ cmdline_bd_code = 'livox0000000001'
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
 cur_config_path = cur_path + '../config'
 rviz_config_path = os.path.join(cur_config_path, 'livox_lidar.rviz')
-user_config_path = os.path.join(cur_config_path, 'livox_lidar_config.json')
+user_port_config_path = os.path.join(cur_config_path, 'minion_livox_port_lidar.json')
+user_center_config_path = os.path.join(cur_config_path, 'minion_livox_center_lidar.json')
+user_sb_config_path = os.path.join(cur_config_path, 'minion_livox_sb_lidar.json')
 ################### user configure parameters for ros2 end #####################
-
-# Define unique frame IDs for each LiDAR
-lidar_frame_map = {
-    "3WEDJAR00100221": "livox_center",
-    "1HDDGC500102611": "livox_port",
-    "1HDDH3200106291": "livox_sb"
-}
 
 def generate_launch_description():
     launch_entities = []
-    
-    # Create nodes for each LiDAR
-    for broadcast_code, frame_id in lidar_frame_map.items():
-        node = Node(
-            package='livox_ros2_driver',
-            executable='livox_ros2_driver_node',
-            name=f'livox_{broadcast_code}_publisher',
-            output='screen',
-            parameters=[
-                {"xfer_format": xfer_format},
-                {"multi_topic": multi_topic},
-                {"data_src": data_src},
-                {"publish_freq": publish_freq},
-                {"output_data_type": output_type},
-                {"frame_id": frame_id},
-                {"lvx_file_path": lvx_file_path},
-                {"user_config_path": user_config_path},
-                {"cmdline_input_bd_code": broadcast_code}  # Changed to use actual broadcast code
-            ]
-        )
-        launch_entities.append(node)
+
+    port_node = Node(
+        package='livox_ros2_driver',
+        executable='livox_ros2_driver_node',
+        name=f'livox_1HDDGC500102611_publisher',
+        output='screen',
+        parameters=[
+            {"xfer_format": xfer_format},
+            {"multi_topic": multi_topic},
+            {"data_src": data_src},
+            {"publish_freq": publish_freq},
+            {"output_data_type": output_type},
+            {"frame_id": "livox_port"},
+            {"lvx_file_path": lvx_file_path},
+            {"user_config_path": user_port_config_path},
+            {"cmdline_input_bd_code": "1HDDGC500102611"}  # Changed to use actual broadcast code
+        ]
+    )
+    launch_entities.append(port_node)
+
+    center_node = Node(
+        package='livox_ros2_driver',
+        executable='livox_ros2_driver_node',
+        name=f'livox_3WEDJAR00100221_publisher',
+        output='screen',
+        parameters=[
+            {"xfer_format": xfer_format},
+            {"multi_topic": multi_topic},
+            {"data_src": data_src},
+            {"publish_freq": publish_freq},
+            {"output_data_type": output_type},
+            {"frame_id": "livox_center"},
+            {"lvx_file_path": lvx_file_path},
+            {"user_config_path": user_center_config_path},
+            {"cmdline_input_bd_code": "3WEDJAR00100221"}  # Changed to use actual broadcast code
+        ]
+    )
+    launch_entities.append(center_node)
+
+    sb_node = Node(
+        package='livox_ros2_driver',
+        executable='livox_ros2_driver_node',
+        name=f'livox_1HDDH3200106291_publisher',
+        output='screen',
+        parameters=[
+            {"xfer_format": xfer_format},
+            {"multi_topic": multi_topic},
+            {"data_src": data_src},
+            {"publish_freq": publish_freq},
+            {"output_data_type": output_type},
+            {"frame_id": "livox_sb"},
+            {"lvx_file_path": lvx_file_path},
+            {"user_config_path": user_sb_config_path},
+            {"cmdline_input_bd_code": "1HDDH3200106291"}  # Changed to use actual broadcast code
+        ]
+    )
+    launch_entities.append(sb_node)
     
     # Return LaunchDescription with all entities
     return LaunchDescription(launch_entities)
